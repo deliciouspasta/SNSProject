a3ccb5c5a5e18244735068271c3e1b08892985db
deliciouspasta <shinyamag@outlook.jp>
2022-03-13 02:41:15 +0900

    delete cach

---
+++ front_end/balloon_front/src/components/HelloWorld.vue
@@ -0,0 +1,14 @@
+<template>
+<div class="header">
+    <b-navbar type="dark" variant="dark">
+      Sign in
+    </b-navbar>
+  </div>
+</template>
+
+<style>
+.header {
+  display: flex;
+  justify-content: flex-end;
+}
+</style>
