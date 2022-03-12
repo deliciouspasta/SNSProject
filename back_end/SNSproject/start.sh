50070ab6783e8167c1d3ec198550e4257eb275c6
deliciouspasta <shinyamag@outlook.jp>
2022-03-13 03:13:45 +0900

    update gitignore

---
+++ back_end/SNSproject/start.sh
@@ -0,0 +1,7 @@
+sleep 5
+cd /
+cd /home/testuser1/dev/SNSproject
+python manage.py makemigrations
+python manage.py migrate
+python manage.py collectstatic --noinput
+uwsgi --socket :8001 --module SNSproject.wsgi -b 32765
---
+++ front_end/start.sh
@@ -0,0 +1,3 @@
+cd /
+cd /app/balloon_front
+npm run serve
