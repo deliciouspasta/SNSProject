260d84ebab4af059144877693f2201f4501c4f9b
deliciouspasta <shinyamag@outlook.jp>
2022-03-22 01:40:12 +0900

    Posts appear randomly

--- front_end/balloon_front/src/components/PostList.vue
+++ front_end/balloon_front/src/components/PostList.vue
@@ -1,6 +1,7 @@
 <template>
   <div>
-    <div v-for="(post, key) in Posts" :key="key">
+    <div v-for="(post, key) in rand_posts" :key="key">
+    <!-- <div v-for="post in rand_posts" :key="post.id"> -->
       <hr>
       <!-- <p>カテゴリ: {{ post.category.name }}</p> -->
   
@@ -27,18 +28,32 @@ export default {
     name: 'PostList',
     data() {
         return {
-            Posts: []
+            Posts: [],
+            rand_posts:[]
         };
     },
     mounted() {
         axios
             .get("http://localhost:8000/back_end/balloon/posts/")
-            .then(response => (this.Posts = response.data))
+            .then(response =>{
+                this.Posts = response.data
+                this.rand_posts = this.shuffle(this.Posts)
+            })
             .catch(error => console.log(error))
     },
     methods:{
         moment: function(date){
             return moment(date).format('YYYY/MM/DD HH:mm:SS')
+        },
+        shuffle: function(rand_posts){
+            for (var i = rand_posts.length -1; i > 0; i--){
+                var j = Math.floor(Math.random() * (i + 1))
+                if (i == j) continue
+                var k = rand_posts[i]
+                rand_posts[i] = rand_posts[j]
+                rand_posts[j] = k
+            }
+            return rand_posts
         }
     }
 };
